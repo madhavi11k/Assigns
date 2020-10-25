90c8ef572c8f9bc9bca3d6c85e1d061f2cbcd3e7
madhavi11k <kolipaka.madhavi1@gmail.com>
2020-10-24 19:26:44 -0500

    Added the imoplimentation for addition

--- Calc/src/com/addition/Add.java
+++ Calc/src/com/addition/Add.java
@@ -1,10 +1,13 @@
 package com.addition;
 
 public class Add {
+	
+	public static int add(int a, int b) {
+		return a+b;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(add(2,4));
 	}
 
 }
